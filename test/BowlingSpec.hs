92f4eefc83ab7d145bab9a84209385f8b6601965
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2018-02-15 22:37:20 +0100

    Add more test cases

--- test/BowlingSpec.hs
+++ test/BowlingSpec.hs
@@ -2,23 +2,33 @@ module BowlingSpec where
 
 import SpecHelper
 
-testScoreGame :: [Int] -> Int -> Spec
-testScoreGame rolls score =
+testScoreGame :: Int -> [Int] -> Spec
+testScoreGame score rolls =
   it (printf " %s => %d \n" (show rolls) score) $
     scoreGame rolls `shouldBe` score
 
 spec :: Spec
 spec = do
   describe "all zeroes" $ do
-    testScoreGame (replicate 20 0) 0
+    testScoreGame 0 (replicate 20 0)
+
   describe "all ones" $ do
-    testScoreGame (replicate 20 1) 20
+    testScoreGame 20 (replicate 20 1)
+
+  describe "all nines and misses" $ do
+    testScoreGame 90 (concat $ replicate 10 [9,0])
+
+  describe "all fives" $ do
+    testScoreGame 150 (replicate 21 5)
+
   describe "one spare" $ do
-    testScoreGame ([5, 5, 3] ++ (replicate 17 0)) 16
+    testScoreGame 16 ([5, 5, 3] ++ (replicate 17 0))
+
   describe "one strike" $ do
-    testScoreGame ([10, 3, 4] ++ (replicate 16 0)) 24
+    testScoreGame 24 ([10, 3, 4] ++ (replicate 16 0))
+
   describe "perfect game" $ do
-    testScoreGame (replicate 12 10) 300
+    testScoreGame 300 (replicate 12 10)
 
 
 main :: IO ()
